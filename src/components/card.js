07aac0be01373e1208063113b656c46a0ab971c2
Ace Chen <12aschen@berkeley.edu>
2024-05-14 17:59:23 -0700

    wandavision page

---
+++ src/components/card.js
@@ -0,0 +1,48 @@
+import { NavLink } from 'react-router-dom';
+// import data from './2019.json';
+// figure out the json stuff ......
+
+import '../App.scss';
+
+export default function Card(props) {
+
+    return (
+
+
+		// NOTE: this is a hardcoded WandaVision one, ver with props below
+		<div>
+			<div className='flex-row gap-36 align-top'>
+				<img src="https://m.media-amazon.com/images/M/MV5BZGEwYmMwZmMtMTQ3MS00YWNhLWEwMmQtZTU5YTIwZmJjZGQ0XkEyXkFqcGdeQXVyMTI5MzA5MjA1._V1_.jpg" alt="" className="card-image"></img>
+				<div className="flex-col gap-12 gray-primary-text">
+					<h1 className='card-title'>WandaVision</h1>
+					<div className='score'>
+						<div className='score-progress green'style={{width:250*50/100}}></div>
+						<div className='score-container'></div>
+						<div className='score-value'>42</div>
+					</div>
+					<p>"WandaVision isn't particularly offensive when it comes to diversity on screen..."</p>
+					<NavLink className={"navLink reviewer"}>Read the review</NavLink>
+				</div>
+			</div>				
+		</div>
+
+
+		/* <div>
+		<div className='flex-row gap-36 align-top'>
+			<img src={props.poster} alt="" className="card-image"></img>
+			<div className="flex-col gap-12 gray-primary-text">
+				<h1 className='card-title'>{props.title}</h1>
+				<div className='score'>
+					<div className='score-progress green'style={{width:250*{props.percentile}/100}}></div>
+					<div className='score-container'></div>
+					<div className='score-value'>{props.score}</div>
+				</div>
+				<p>"{props.teext}"</p>
+				<NavLink className={"navLink reviewer"} to={props.reviewer}>Read the review</NavLink>
+			</div>
+		</div>				
+		</div> */
+
+    )
+
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import AboutMERL from '../components/aboutMERL';
 import Articles from '../components/articles'
 import Collection from '../components/collection';
 import Footer from '../components/footer';
+import { NavLink } from 'react-router-dom';
 import '../App.scss';
 
 const Home = () => {
@@ -17,7 +18,16 @@ const Home = () => {
 				<LatestReviews/>
 				<AboutMERL/>
 				<Articles/>
-				<Collection/>
+				
+				<div className='flex-row space-between'>
+					<div className="flex-row gap-12">
+						<span class="dot blue"></span>
+						<h1>MERL's Top 10</h1>	
+					</div>
+					<NavLink id="view-collection" to="/top10">VIEW COLLECTION</NavLink>
+				</div>
+				<Collection className='section-margin'/>
+				
 				<Footer/>
 			</body>
 		</div>
