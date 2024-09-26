380d4460bf8dcc49461b4faa9c9f933fb85ba7ad
Ace Chen <12aschen@berkeley.edu>
2024-09-25 23:55:17 -0700

    updated navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,20 +7,35 @@ export default function Navbar() {
     return (
 		<div className='flex-row' id='nav'>
 			<div className='nav-section'>
-				<NavLink className={"navLink"} to="/search">Reviews</NavLink>
-				<NavLink className={"navLink"} to="/">Articles</NavLink>
-				<NavLink className={"navLink"} to="/">Rating System</NavLink>
+				<div className='navItem'>
+					<NavLink to="/search">Reviews</NavLink>
+				</div>
+				<div className='navItem'>
+					<NavLink to="/">Articles</NavLink>
+				</div>
+				<div className='navItem'>
+					<NavLink to="/">About MERL</NavLink>
+				</div>
+				<div className='navItem'>
+					<NavLink to="/">TSB System</NavLink>
+				</div>
+				<div className='navItem'>
+					<NavLink to="/">For Education</NavLink>
+				</div>
+				<div className='navItem'>
+					<NavLink to="/">Sign In</NavLink>
+				</div>
 			</div>
-			<div className='nav-section flex-col'>
+			{/* <div className='nav-section flex-col'>
 				<a href="/"><img src={logo} alt="MERL Logo" id="navLogo" width={300}></img></a>
 				<h2>Diversity scores for film and TV</h2>
 				
-			</div>
-			<div className='nav-section'>
+			</div> */}
+			{/* <div className='nav-section'>
 				<input type="text" placeholder="Search film and TV" className='text-input'></input>
 				<NavLink className={"navLink"} to="/">Sign In</NavLink>
 				
-			</div>
+			</div> */}
 		</div>
     )
 
