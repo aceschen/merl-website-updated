02574942904ad27f715d77152d47ef8cc2220f51
Ace Chen <12aschen@berkeley.edu>
2025-03-10 12:36:17 -0700

    whitespace fix

--- src/components/indvtext/reviews.js
+++ src/components/indvtext/reviews.js
@@ -10,14 +10,16 @@ export default function Reviews(props) {
 		<div className='align-left section-header white reviews'>
 			<h1>Review #1</h1>
 			<h3>Posted ADD DATE by <b>{reviewer1.pseudonym ? reviewer1.pseudonym : reviewer1.tagger}</b></h3>
-			<p dangerouslySetInnerHTML={{ __html: reviewer1.copyEditedBlurb }}/>
+			<p style={{ whiteSpace: 'pre-wrap' }} 
+			dangerouslySetInnerHTML={{ __html: reviewer1.copyEditedBlurb }}/>
 
 			<br/>
 			<br/>
 			
 			<h1>Review #2</h1>
 			<h3>Posted ADD DATE by <b>{reviewer2.pseudonym ? reviewer2.pseudonym : reviewer2.tagger}</b></h3>
-			<p dangerouslySetInnerHTML={{ __html: reviewer2.copyEditedBlurb }}/>
+			<p  style={{ whiteSpace: 'pre-wrap' }} 
+			dangerouslySetInnerHTML={{ __html: reviewer2.copyEditedBlurb }}/>
 			
 		</div>
     )
